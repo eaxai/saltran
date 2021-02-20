eefdc140641d70b9264052fe9c99038d7edbf3f4
Ekrem Aksoy <ekremaksoy@gmail.com>
2021-02-20 17:49:05 +0300

    WIP

--- model.py
+++ model.py
@@ -1,194 +1,70 @@
 import numpy as np
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+import torchvision
 
 '''
-Input  -> FeatExtract -> Resnet18/VGG16 -> PosEncode -> MHSA -> FFN -----I
-Output ->                               -> PosEncode -> MHSA --------> MHSA -> FFN -> Output (org. Linear -> Softmax, we might need a 1x1 conv or something like that)
+Input  -> FeatExtract -> VGG16 -> PosEncode -> MHSA -> FFN -----I
+Output ->                      -> PosEncode -> MHSA --------> MHSA -> FFN -> Output (org. Linear -> Softmax, we might need a 1x1 conv or something like that)
 '''
-
-
-class PositionwiseFeedForward(nn.Module):
-    ''' A two-feed-forward-layer module '''
-
-    def __init__(self, d_in, d_hid, dropout=0.1):
-        super().__init__()
-        self.w_1 = nn.Linear(d_in, d_hid) # position-wise
-        self.w_2 = nn.Linear(d_hid, d_in) # position-wise
-        self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x):
-
-        residual = x
-
-        x = self.w_2(F.relu(self.w_1(x)))
-        x = self.dropout(x)
-        x += residual
-
-        x = self.layer_norm(x)
-
-        return x
-
-
-class ScaledDotProductAttention(nn.Module):
-    ''' Scaled Dot-Product Attention '''
-
-    def __init__(self, temperature, attn_dropout=0.1):
-        super().__init__()
-        self.temperature = temperature
-        self.dropout = nn.Dropout(attn_dropout)
-
-    def forward(self, q, k, v, mask=None):
-
-        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
-
-        if mask is not None:
-            attn = attn.masked_fill(mask == 0, -1e9)
-
-        attn = self.dropout(F.softmax(attn, dim=-1))
-        output = torch.matmul(attn, v)
-
-        return output, attn
-
-
-class PositionalEncoding(nn.Module):
-    def __init__(self):
-        pass
-
-    def _get_angles(pos, i, d_model):
-        angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
-        return pos * angle_rates
-
-    def positional_encoding(position, d_model):
-        angle_rads = _get_angles(np.arange(position)[:, np.newaxis],
-                                np.arange(d_model)[np.newaxis, :],
-                                d_model)
-
-        # apply sin to even indices in the array; 2i
-        angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
-
-        # apply cos to odd indices in the array; 2i+1
-        angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
-
-        pos_encoding = angle_rads[np.newaxis, ...]
-
-        return pos_encoding
-
-    def forward(position, d_model):
-        pass
-
-
-class MultiHeadAttention(nn.Module):
-    ''' Multi-Head Attention module '''
-
-    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
-        super().__init__()
-
-        self.n_head = n_head
-        self.d_k = d_k
-        self.d_v = d_v
-
-        self.w_qs = nn.Linear(d_model, n_head * d_k, bias=False)
-        self.w_ks = nn.Linear(d_model, n_head * d_k, bias=False)
-        self.w_vs = nn.Linear(d_model, n_head * d_v, bias=False)
-        self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
-
-        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
-
-        self.dropout = nn.Dropout(dropout)
-        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
-
-
-    def forward(self, q, k, v, mask=None):
-
-        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
-        sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
-
-        residual = q
-
-        # Pass through the pre-attention projection: b x lq x (n*dv)
-        # Separate different heads: b x lq x n x dv
-        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
-        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
-        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
-
-        # Transpose for attention dot product: b x n x lq x dv
-        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
-
-        if mask is not None:
-            mask = mask.unsqueeze(1)   # For head axis broadcasting.
-
-        q, attn = self.attention(q, k, v, mask=mask)
-
-        # Transpose to move the head dimension back: b x lq x n x dv
-        # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
-        q = q.transpose(1, 2).contiguous().view(sz_b, len_q, -1)
-        q = self.dropout(self.fc(q))
-        q += residual
-
-        q = self.layer_norm(q)
-
-        return q, attn
-
-
-class EncoderLayer(nn.Module):
-    ''' Compose with two layers '''
-
-    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
-        super(EncoderLayer, self).__init__()
-        self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
-        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
-
-    def forward(self, enc_input, slf_attn_mask=None):
-        enc_output, enc_slf_attn = self.slf_attn(
-            enc_input, enc_input, enc_input, mask=slf_attn_mask)
-        enc_output = self.pos_ffn(enc_output)
-        return enc_output, enc_slf_attn
-
-
-class DecoderLayer(nn.Module):
-    ''' Compose with three layers '''
-
-    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
-        super(DecoderLayer, self).__init__()
-        self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
-        self.enc_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
-        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
-
-    def forward(
-            self, dec_input, enc_output,
-            slf_attn_mask=None, dec_enc_attn_mask=None):
-        dec_output, dec_slf_attn = self.slf_attn(
-            dec_input, dec_input, dec_input, mask=slf_attn_mask)
-        dec_output, dec_enc_attn = self.enc_attn(
-            dec_output, enc_output, enc_output, mask=dec_enc_attn_mask)
-        dec_output = self.pos_ffn(dec_output)
-        return dec_output, dec_slf_attn, dec_enc_attn
-
 
 class FeatureExtractor(nn.Module):
     def __init__(self):
-        pass
+        super(FeatureExtractor, self).__init__()
+        m = torchvision.models.vgg16(pretrained=True)
+        for param in m.parameters():
+            param.requires_grad = False
+        del m.avgpool
+        del m.classifier
+        del m.layers[30]
+        md = list(m.features)
+        md.insert(24, torch.nn.ZeroPad2d((0,1,0,1)))
+        m.features = torch.nn.Sequential(*md)
+        m.features[23].stride=1
+        m.features[25].padding = (2,2)
+        m.features[25].dilation = (2,2)
+        m.features[27].padding = (2,2)
+        m.features[27].dilation = (2,2)
+        m.features[29].padding = (2,2)
+        m.features[29].dilation = (2,2)
+        self.vgg = m.features
 
-    def forward(x):
-        pass
 
-class Encoder(nn.Module):
-    def __init__(self):
-        pass
+    def forward(self, data):
+        out = self.vgg(data)
+        return out
+
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
 
     def forward(self, x):
-        pass
-
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
 class SalTran(nn.Module):
-    def __init__(self, x):
-        self.posenc = PositionalEncoding(x, n_position=n_position)
-        self.encoder = [] # output should be 30x40x1
-        self.decoder = [] # output should be 30x40x1
-        pass
+    def __init__(self):
+        super(SalTran, self).__init__()
+        self.vgg = FeatureExtractor() # VGG16-based feature extractor
+        self.pe = PositionalEncoding(1200)
+        enc_layer = nn.TransformerEncoderLayer(512, 8)
+        self.enc = nn.TransformerEncoder(enc_layer, 6)
+        self.dec =
+
+
+
+
 
     def forward(self, x, mask=None):
-        pass
+        src =  self.pe( )
